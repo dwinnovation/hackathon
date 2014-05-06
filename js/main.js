0f7025661db7cdfc6ce5df26d197bbe7b1eb8a00
dwinnovation <hannes.rohde@dw.de>
2014-05-06 12:33:50 +0200

    calculation for scoring

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-
+/**
+ * input:
+ *   guessed: array of number
+ *   correct: array of number
+ * output:
+ *   average absolute deviation between guessed and correct
+ */
+function averageDeviation(guessed, correct) {
+	var sum = 0;
+	for (var i=0; i<guessed.length; i++) {
+		sum += Math.abs(guessed[i]-correct[i]);
+	}
+	return sum / guessed.length;
+}
