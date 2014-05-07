578ee3b0dd0be8cb604e239eaf8a2b321b0091b8
dwinnovation <rebekka.drobbe@dw.de>
2014-05-07 13:55:20 +0200

    new json content

--- js/productData.js
+++ js/productData.js
@@ -11,14 +11,14 @@
             profit: 56,
             marketing: 78
         },
-        texts: {
-            wages: "Rund acht Prozent des Preises eines iPhones gehen für die Fertigungskosten des Geräts drauf.",
-            materials: "Das Material eines iPhones beschlagnahmt rund 20 Prozent seines Gesamtpreises.",
-            logistics: "Fünf Prozent der Kosten beträgt der Anteil für die Logistik.",
-            taxes: "Die Mehrwertsteuer in Deutschland beträgt 19 Prozent.",
-            profit: "Über 55 Prozent der Gesamtkosten eines iPhones gehen in den Gewinn über.",
-            marketing: "Die Marketingkosten machen bei einem iPhone etwa 0,5 Prozent des Preises aus."
-        }
+        texts:{
+           wages : "Apple’s phone is a status symbol - not so much though for those assembling it. Suppliers in China like Foxconn are known for their bad working conditions, low wages and high suicide rates.",
+           materials : "Phones have a huge set of features nowadays. Therefore, they need top quality components and raw material, for exampe rare earths - the latter being hard to harvest and therefor very expensive.",
+           logistics : "The more you ship, the cheaper it gets for each item. Apple is a global player selling it’s products all over the world, but they do sell a lot - which leads to a powerful position when negotioating with logistics companies.",
+           taxes : "Multinationals like Apple use very sophisticated systems to avoid paying taxes. Apple for example uses a system that is called the \"Double Irish with Dutch Sandwich\" and involves branches in Ireland and the Netherlands.",
+           profit : "More than half of the retail price goes into Apple’s cash stash. That made it the wealthiest company in the world, with a war chest worth almost 160 billion US-Dollars.",
+           marketing : "Apple is known for it’s ad campaigns - compared to other tech companies, they are pretty tightfisted with their marketing budget, however. They spend a mere 1.1 Billion USD, competitor Samsung for example spends 14 Billion USD."
+       }
     },
     {
         name: "1l Benzin",
